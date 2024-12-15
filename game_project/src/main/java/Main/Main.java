52daf0cd5f4388d88cae29ab02c667ad61f1a88a
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-12-15 21:08:38 +0300

    added frame_counter

--- game_project/src/main/java/Main/Main.java
+++ game_project/src/main/java/Main/Main.java
@@ -1,16 +1,41 @@
 package Main;
+
 import engine.Window;
 
-public class Main 
+public class Main implements Runnable
 {
-    public static void main(String[] args) {
-        Window window = new Window(800, 600, "Hello");
-        window.create();
-
-        while(!window.closed()){
-            window.update();
-            System.out.println("hey");
-            window.swapBuffers();
+    public Thread game;
+    public static Window window;
+    public static final int WIDTH = 1280, HEIGHT = 720; // 1920 1010
+    
+        public void start(){
+            game = new Thread(this, "game");
+            game.start();
         }
+    
+        public static void init(){
+            System.out.println("Initializing the Game!");
+            window = new Window(WIDTH, HEIGHT, "Game");
+            window.create();
+    }
+
+    public void run(){
+        init();
+        while (!window.closed()) { 
+            update();
+            render();   
+        }
+    }
+    private void update(){
+        //System.out.println("Updating the Game!");
+        window.update();
+    }
+    
+    private void render(){
+        //System.out.println("Rendering the Game!");
+        window.swapBuffers();
+    }
+    public static void main(String[] args) {
+        new Main().start();
     }
 }
--- game_project/src/main/java/engine/Window.java
+++ game_project/src/main/java/engine/Window.java
@@ -8,6 +8,8 @@ public class Window
     private int width = 1280, height = 720;
     private String title;
     private long window;
+    public int frames;
+    public static long time;
 
     public Window(int width, int height, String title)
     {
@@ -18,8 +20,8 @@ public class Window
 
     public void create(){
         if(!GLFW.glfwInit()){
-            System.err.println("Error: Couldn't initialize glfw");
-            System.exit(-1);
+            System.err.println("ERROR: Couldn't initialize GLFW");
+            return;
         }
         // Settings of screen
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
@@ -27,13 +29,19 @@ public class Window
         window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
         
         if (window == 0){
-            System.err.println("Error: Window Couldn't be created.");
-            System.exit(-1);
+            System.err.println("ERROR: Window Couldn't be created.");
+            return;
         }
+        
         GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
         GLFW.glfwSetWindowPos(window, (videoMode.width() - width) / 2, (videoMode.height() - height) / 2);
-    
+        GLFW.glfwMakeContextCurrent(window);
+
         GLFW.glfwShowWindow(window);
+
+        GLFW.glfwSwapInterval(1); // its giving gap between two interwals of swap which is frames
+        
+        time = System.currentTimeMillis();
     }
 
     public boolean closed(){
@@ -42,6 +50,12 @@ public class Window
     
     public void update(){// Gets rid of everything from previous frame
         GLFW.glfwPollEvents();
+        frames++;
+        if(System.currentTimeMillis() > time + 1000){
+            GLFW.glfwSetWindowTitle(window, title + " | FPS: " + frames );
+            time = System.currentTimeMillis();
+            frames = 0;
+        }
     }
 
     public void swapBuffers(){// Renders necessary things for screen

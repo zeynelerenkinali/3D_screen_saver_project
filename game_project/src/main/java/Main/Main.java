6c1d970343b23f3984cdf58672cdb2f851a3f1bb
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-12-26 19:12:36 +0300

    first person added

--- game_project/src/main/java/Main/Main.java
+++ game_project/src/main/java/Main/Main.java
@@ -24,16 +24,68 @@ public class Main implements Runnable
     public final int WIDTH = 1280, HEIGHT = 720; // 1920 1010
     
     public Mesh mesh = new Mesh(new Vertex[] {
-		    new Vertex(new Vector3f( -0.5f,0.5f, 0.0f), new Vector3f(1.0f, 0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
-			new Vertex(new Vector3f( -0.5f, -0.5f, 0.0f), new Vector3f(0.5f, 0.3f, 1.0f), new Vector2f(0.0f, 1.0f)),
-			new Vertex(new Vector3f(0.5f, -0.5f, 0.0f), new Vector3f(1.0f, 0.1f, 0.0f), new Vector2f(1.0f, 1.0f)),
-			new Vertex(new Vector3f(0.5f,0.5f, 0.0f), new Vector3f(0.0f, 0.5f, 1.0f), new Vector2f(1.0f, 0.0f))
-        }, new int[] {
-            0, 1, 2,
-            0, 3, 2, 
-        }, new Material("game_project/src/main/resources/textures/dvd_video.png"));
+        //Back face
+        new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+        
+        //Front face
+        new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+        
+        //Right face
+        new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+        
+        //Left face
+        new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+        
+        //Top face
+        new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+        
+        //Bottom face
+        new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 0.0f)),
+        new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(0.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 1.0f)),
+        new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f),new Vector3f(1.0f,  0.5f, 0.1f), new Vector2f(1.0f, 0.0f)),
+    }, new int[] {
+            //Back face
+            0, 1, 3,	
+            3, 1, 2,	
+            
+            //Front face
+            4, 5, 7,
+            7, 5, 6,
+            
+            //Right face
+            8, 9, 11,
+            11, 9, 10,
+            
+            //Left face
+            12, 13, 15,
+            15, 13, 14,
+            
+            //Top face
+            16, 17, 19,
+            19, 17, 18,
+            
+            //Bottom face
+            20, 21, 23,
+            23, 21, 22
+    }, new Material("game_project/src/main/resources/textures/dvd_video.png"));
 
-        public GameObject object = new GameObject(mesh, new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), new Vector3f(0.25f, 0.25f, 1.0f));
+        public GameObject object = new GameObject(mesh, new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), new Vector3f(1.0f, 1.0f, 1.0f));
 
         public Camera camera = new Camera(new Vector3f(0, 0, 1), new Vector3f(0, 0, 0));
         
@@ -59,6 +111,7 @@ public class Main implements Runnable
             update();
             render();
             if(Input.isKeyDown(GLFW.GLFW_KEY_F11)){ window.setFullScreen(!window.isFullScreen()); Input.setKey(GLFW.GLFW_KEY_F11, false);}
+            if(Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) window.mouseState(true);
         }
         close();
     }
--- game_project/src/main/java/engine/io/Window.java
+++ game_project/src/main/java/engine/io/Window.java
@@ -154,6 +154,10 @@ public class Window
         }
     }
 
+    public void mouseState(boolean lock){ 
+        GLFW.glfwSetInputMode(window, GLFW.GLFW_CURSOR, lock ? GLFW.GLFW_CURSOR_DISABLED : GLFW.GLFW_CURSOR_NORMAL);
+    }
+
     public Matrix4f getProjectionMatrix() {
         return projection;
     }
